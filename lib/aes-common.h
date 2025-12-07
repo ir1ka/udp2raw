b0b5751b74e6a9c358d9fb3cf128cf4481140e80
IriKa Qiu <qiujie.jq@gmail.com>
2025-12-07 18:52:06 +0000

    Use cmake build amd64 hw aes
    Signed-off-by: IriKa Qiu <qiujie.jq@gmail.com>

--- lib/aes-common.h
+++ lib/aes-common.h
@@ -6,6 +6,9 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void AES_ECB_encrypt_buffer(const uint8_t* input, const uint8_t* key, uint8_t *output);
 void AES_ECB_decrypt_buffer(const uint8_t* input, const uint8_t* key, uint8_t *output);
@@ -16,3 +19,7 @@ void AES_CBC_decrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, co
 
 void AES_CFB_encrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
 void AES_CFB_decrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
+
+#ifdef __cplusplus
+}
+#endif
